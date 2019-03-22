6d80fb69cbf66225d99b1e93ed7b3e7daca3d9b3
Fredo Raherimanjakaniaina <fredo.niaina@gmail.com>
2019-03-22 15:25:28 +0300

    Implémentation du Visite virtuelle

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+$title="Accueil";
+$object="Bienvenue sur WA Creator";
+
+include ('libs/wac_header.php');
+?>
+
+
+<div class="main">
+	<div class="row-1">
+		<h1 class="header-row">Présentation</h1>
+		<div class="body-row">
+			<p>Nous sommes un groupe de <i>freelance</i> proposant venant principalement de <i>Madagascar</i></p>
+			<p>Nous exercons dans le domaine du Web et de l'informatique en générale. Voyez <a href="wac_services.php">nos services ici!</a></p>
+		</div>
+	</div>
+
+	<div class="row-1">
+		<H1 class="header-row">Les technologies que nous utilisons</H1>
+		<div class="body-row">
+			
+		</div>
+	</div>
+</div>
+
+<?php include('libs/wac_footer.php') ?>
+
+
+
+
+
