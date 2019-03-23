85019aaf227a6e402df6c9c39fed10b82c58258f
Fredo Raherimanjakaniaina <fredo.niaina@gmail.com>
2019-03-23 11:42:36 +0300

    Dashboard

--- index.php
+++ index.php
@@ -7,18 +7,23 @@ include ('libs/wac_header.php');
 
 
 <div class="main">
-	<div class="row-1">
-		<h1 class="header-row">Présentation</h1>
-		<div class="body-row">
+	<div class="column">
+		<h1 class="col-header">Présentation</h1>
+		<div class="col-body">
 			<p>Nous sommes un groupe de <i>freelance</i> proposant venant principalement de <i>Madagascar</i></p>
 			<p>Nous exercons dans le domaine du Web et de l'informatique en générale. Voyez <a href="wac_services.php">nos services ici!</a></p>
 		</div>
 	</div>
 
-	<div class="row-1">
-		<H1 class="header-row">Les technologies que nous utilisons</H1>
-		<div class="body-row">
-			
+	<div class="column">
+		<h1 class="col-header">Les technologies que nous utilisons</h1>
+		<div class="col-body">
+			<p>Nous utilisons, principalement</p>
+			<ul>
+				<li>PHP : pour les sites dynamiques et la sécurisation des programmes</li>
+				<li>HTML & CSS : mise en place des designs et des styles</li>
+				<li>JavaScript : pour divers traitement, animation ...</li>
+			</ul>
 		</div>
 	</div>
 </div>
