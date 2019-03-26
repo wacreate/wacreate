5780e37443360d847a9e8836c8b7571acafcad88
Fredo Raherimanjakaniaina <fredo.niaina@gmail.com>
2019-03-26 19:05:31 +0300

    Navbar 2.0

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+$title="Accueil";
+$object="Développeur Web Freelance";
+
+include ('libs/wac_header.php');
+?>
+
+<section class="main">
+	<div class="colonne">
+		<h1 class="col-header">Présentation</h1>
+		<div class="col-body">
+			<span class="spcword">Hello World !</span> <br>
+
+			<p>Le challenge! J'aime le challenge. <br>
+			Vous avez un projet Web à faire? Vous avez besoin d'un site web? Besoin d'un nouveau module sur votre site web ? <br>
+			Je suis là pour vous servir et vous aider, n'hésitez plus! Engagez-vous avec un développeur jeune et dynamique.</p>
+
+		</div>
+	</div>
+</section>
+
+<?php include('libs/wac_footer.php') ?>
