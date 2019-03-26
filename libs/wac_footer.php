fd73a4a452c80ecac0ea60d4c4841561670a00a5
Fredo Raherimanjakaniaina <fredo.niaina@gmail.com>
2019-03-26 19:19:02 +0300

    First change : footer

---
+++ libs/wac_footer.php
@@ -0,0 +1,12 @@
+<footer class="footer-fwd">
+	<div>
+		<ul id="footer-fw">
+			<li><a href="#top"><i class="fas fa-angle-double-up"></i></a></li>
+			<li> (c)Tous droits réservés <a href="">Fredo Raherimanjakaniaina</a></li>
+			<li>
+				
+			</li>
+		</ul>
+	</div>
+</footer>
+</html>
