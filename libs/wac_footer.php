5f181d0568cb3f596b66001253fd5a8a2ed66615
Fredo Raherimanjakaniaina <fredo.niaina@gmail.com>
2019-03-26 19:20:36 +0300

    Footer

--- libs/wac_footer.php
+++ libs/wac_footer.php
@@ -2,9 +2,17 @@
 	<div>
 		<ul id="footer-fw">
 			<li><a href="#top"><i class="fas fa-angle-double-up"></i></a></li>
-			<li> (c)Tous droits réservés <a href="">Fredo Raherimanjakaniaina</a></li>
+			<li> (c)Tous droits réservés <span class="spcword">Fredo Raherimanjakaniaina</span>  
+				<a href="https://web.facebook.com/fredowd">Facebook</a>
+			</li>
 			<li>
-				
+				<a href="">LinkedIn</a>	
+			</li>
+			<li>
+				<a href="https://github.com/frniaina">GitHub</a>
+			</li>
+			<li>
+				<a href="https://wacreator.slack.com">Slack</a>
 			</li>
 		</ul>
 	</div>
