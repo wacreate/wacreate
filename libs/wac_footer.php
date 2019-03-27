b40a941e9ec6587444dbdc3d16131fad017d9cd4
Fredo Raherimanjakaniaina <fredo.niaina@gmail.com>
2019-03-27 10:49:58 +0300

    Footer - avec Logo

--- libs/wac_footer.php
+++ libs/wac_footer.php
@@ -5,15 +5,18 @@
 		<ul id="footer-fw">
 			<li><a href="#top"><i class="fas fa-angle-double-up"></i></a></li>
 			<li> (c)Tous droits réservés <span class="spcword">Fredo Raherimanjakaniaina</span>  
-				<a href="https://web.facebook.com/fredowd">Facebook</a>
+				<a href="https://web.facebook.com/fredowd"><i class="fab fa-facebook"></i>Facebook</a>
 			</li>
 			<li>
+				<i class="fab fa-linkedin"></i>
 				<a href="">LinkedIn</a>	
 			</li>
 			<li>
+				<i class="fab fa-github"></i>
 				<a href="https://github.com/frniaina">GitHub</a>
 			</li>
 			<li>
+				<i class="fab fa-slack"></i>
 				<a href="https://wacreator.slack.com">Slack</a>
 			</li>
 		</ul>
